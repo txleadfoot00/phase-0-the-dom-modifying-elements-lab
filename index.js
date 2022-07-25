c217b77bac5a3857a3ce5fb384e35aec49db9935
txleadfoot00 <106290919+txleadfoot00@users.noreply.github.com>
2022-07-24 22:40:18 -0700

    commit tree 64d7c206dc85aa2809ade661df5f9f0295003dd8

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "<h1> Roger is the champion </h1>";
+document.body.append(newHeader);
